98f814a0c052188ebf31f87491cd63cfe556574c
jamatov <mirabdullajamatov@gmail.com>
2022-10-30 17:51:10 +0500

    s

---
+++ src/components/Homecards.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Homecards() {
+  return (
+    <div className='Homecards'>
+      <h2>Смотрите Кинопоиск на телевизоре</h2>
+
+      <div className='home-cards'>
+        <div className='item1'></div>
+        <div className='item2'></div>
+        <div className='item3'></div>
+        <div className='item4'></div>
+        <div className='item5'></div>
+        <div className='item6'></div>
+        <div className='item7'></div>
+
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Homecards from '../components/Homecards'
 import Yandexplus from '../components/Yandexplus'
 
 export default function Home() {
   return (
     <div className='Home'>
       <Yandexplus/>
+      <Homecards/>
     </div>
   )
 }
