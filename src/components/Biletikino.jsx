7a9639870ece6c9ad42230bd5cb974ce75a2214b
jamatov <mirabdullajamatov@gmail.com>
2022-11-01 21:11:41 +0500

    sd

--- src/components/Biletikino.jsx
+++ src/components/Biletikino.jsx
@@ -2,70 +2,40 @@ import React from 'react'
 import Slider from "react-slick";
 
 export default function Biletikino() {
-  var settings = {
+  const settings = {
+    className: "slider variable-width",
     dots: true,
-    infinite: false,
-    speed: 700,
-    slidesToShow: 4,
-    slidesToScroll: 3,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 6,
+    variableWidth: true
   };
   return (
     <div className='Biletikino'>
-      <div>
+      <div className='container2'>
         <h2> Билеты в кино </h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+        <Slider className='slider' {...settings}>
+          <div className='slider__item' style={{ width: 460 }}>
+            <p>100</p>
           </div>
-          <div>
-            <h3>2</h3>
+          <div className='slider__item' style={{ width: 140 }}>
+            <p>200</p>
           </div>
-          <div>
-            <h3>3</h3>
+          <div className='slider__item' style={{ width: 140}}>
+            <p>75</p>
           </div>
-          <div>
-            <h3>4</h3>
+          <div className='slider__item' style={{ width: 140 }}>
+            <p>300</p>
           </div>
-          <div>
-            <h3>5</h3>
+          <div className='slider__item' style={{ width: 140 }}>
+            <p>225</p>
           </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
+          <div className='slider__item' style={{ width: 140 }}>
+            <p>175</p>
           </div>
         </Slider>
+
       </div>
     </div>
   )
